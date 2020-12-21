11455b9c0c01be130960160da33a00912e087071
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-21 20:39:26 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
-const cocktail = require("./cocktails.js");
+const cocktail = require("./cocktail.js");
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
